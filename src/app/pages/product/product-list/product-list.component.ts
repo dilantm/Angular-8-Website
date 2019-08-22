23a07901d0b64056f456f47239d3d4c3c216f97c
Dilan <dilantm@gmail.com>
2019-08-23 01:13:08 +0530

    add new product

--- src/app/pages/product/product-list/product-list.component.ts
+++ src/app/pages/product/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
   componentTitle = 'Product List';
+  rating = Array;
   products: any[] = [
     {
     'productId': 1,
@@ -15,8 +16,10 @@ export class ProductListComponent implements OnInit {
     'releaseDate': 'March 19, 2019',
     'description': 'Leaf rake with 48-inch wooden handle.',
     'price': 19.95,
+    'PrePrice': 22.00,
     'starRating': 3.2,
-    'imageUrl': 'assets/images/leaf_rake.png'
+    'imageUrl': 'http://bestjquery.com/tutorial/product-grid/demo9/images/img-1.jpg',
+    'imageUrl2': 'http://bestjquery.com/tutorial/product-grid/demo9/images/img-2.jpg',
   },
   {
     'productId': 2,
@@ -25,8 +28,34 @@ export class ProductListComponent implements OnInit {
     'releaseDate': 'March 18, 2019',
     'description': '15 gallon capacity rolling garden cart',
     'price': 32.99,
+    'PrePrice': 35.00,
     'starRating': 4.2,
-    'imageUrl': 'assets/images/garden_cart.png'
+    'imageUrl': 'http://bestjquery.com/tutorial/product-grid/demo6/images/img-7.jpg',
+    'imageUrl2': 'http://bestjquery.com/tutorial/product-grid/demo6/images/img-8.jpg'
+  },
+  {
+    'productId': 2,
+    'productName': 'Garden Cart',
+    'productCode': 'GDN-0023',
+    'releaseDate': 'March 18, 2019',
+    'description': '15 gallon capacity rolling garden cart',
+    'price': 20.99,
+    'PrePrice': 28.00,
+    'starRating': 4.8,
+    'imageUrl': 'http://bestjquery.com/tutorial/product-grid/demo73/images/img-3.jpg',
+    'imageUrl2': 'http://bestjquery.com/tutorial/product-grid/demo73/images/img-4.jpg'
+  },
+  {
+    'productId': 2,
+    'productName': 'Garden Cart',
+    'productCode': 'GDN-0023',
+    'releaseDate': 'March 18, 2019',
+    'description': '15 gallon capacity rolling garden cart',
+    'price': 18.99,
+    'PrePrice': 20.00,
+    'starRating': 4.2,
+    'imageUrl': 'http://bestjquery.com/tutorial/product-grid/demo58/images/img-5.jpg',
+    'imageUrl2': 'http://bestjquery.com/tutorial/product-grid/demo58/images/img-6.jpg'
   }];
   constructor() { }
 
